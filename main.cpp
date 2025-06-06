456aa6953d3532e48c4d2b409a1bb57b057ee6e9
Hari Venkat <hariv7790@gmail.com>
2025-06-06 06:00:21 -0500

    commitNumeroDos

--- main.cpp
+++ main.cpp
@@ -3,14 +3,28 @@
 using namespace std;
 
 int main(){
-    int x, y;
+    int x, y, z;
     cin>>x;
-    vector<int> array(x);
+    vector<int> row1(x);
+    vector<int> row2(x);
+    int position1, position2;
     for(int count=0;count<x;count++){
-        cin>>y;
-        array.push_back(y);
+        cin>>row1[count];
+        
     }
-    
+    for(int count=0;count<x;count++){
+        cin>>row2[count];
+    }
+    for(int count=0;count<x;count++){
+        if(row2[count]==1){
+            position1=count;
+
+        }else if(row2[count]==4){
+            position2=count;
+
+        }
+    }
+    cout<<position1<<" "<<position2;
  
 
     return 0;
