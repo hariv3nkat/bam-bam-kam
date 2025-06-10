16b92a1ad120ffad84e4d6549ad741a2f9833ed8
Hari Venkat <hariv7790@gmail.com>
2025-06-10 09:56:00 -0500

    jaa

--- main.cpp
+++ main.cpp
@@ -5,11 +5,11 @@ int main(){
     int n;
     cin>>n;
     vector<int> gays(5);
-    
+
     for(int i=0;i<5;i++){
         cin>>gays[i];
     }
-    cout<<gays[n];
+    cout<<gays[7];
     
     
     return 0;
