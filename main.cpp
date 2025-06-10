8cd78eb3ee8b418f24d3e448c28ff21658242af7
Hari Venkat <hariv7790@gmail.com>
2025-06-10 09:51:27 -0500

    realFirstCOmmit

--- main.cpp
+++ main.cpp
@@ -1,50 +1,15 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int kthDivisible(int a, int b, int d, int k){
-    vector<int> arrays;
-    for(int i=a; i<b; i++){
-        if(i%d==0){
-            arrays.push_back(i);
-        }
-    }
-    cout<<arrays[k];
-}
-void tournament(int crazys){
-    
-
-}
-
 int main(){
+    int n;
+    cin>>n;
     vector<int> gays(5);
-    int a,b;
-    long long great, bang;
-    great=1;
-    bang=0;
-    cin>>a>>b;
-    bool flagg=false;
     
-    for(int e=a+1;e<=62;e++){
-        for(int count=0;count<e;count++){
-            great*=2;
-        }
-        bang=great;
-        while(bang>=10){
-            bang/=10;
-        }
-        if(bang==b){
-            flagg=true;
-            cout<<e;
-            break;
-        }else{
-            great=1;
-        }
-        
+    for(int i=0;i<5;i++){
+        cin>>gays[i];
     }
-    if(flagg==false){
-        cout<<0;
-    }
-    
+    cout<<gays[n];
     
     
     return 0;
