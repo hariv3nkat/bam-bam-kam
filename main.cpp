1908d37ba4b087bf43911bb4b52aa81deaf486d0
Hari Venkat <hariv7790@gmail.com>
2025-06-10 09:36:01 -0500

    june tenth

--- main.cpp
+++ main.cpp
@@ -1,31 +1,51 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
-int main(){
-    int x, y, z;
-    cin>>x;
-    vector<int> row1(x);
-    vector<int> row2(x);
-    int position1, position2;
-    for(int count=0;count<x;count++){
-        cin>>row1[count];
-        
-    }
-    for(int count=0;count<x;count++){
-        cin>>row2[count];
-    }
-    for(int count=0;count<x;count++){
-        if(row2[count]==1){
-            position1=count;
-
-        }else if(row2[count]==4){
-            position2=count;
-
+int kthDivisible(int a, int b, int d, int k){
+    vector<int> arrays;
+    for(int i=a; i<b; i++){
+        if(i%d==0){
+            arrays.push_back(i);
         }
     }
-    cout<<position1<<" "<<position2;
- 
+    cout<<arrays[k];
+}
+void tournament(int crazys){
+    
 
+}
+
+int main(){
+    vector<int> gays(5);
+    int a,b;
+    long long great, bang;
+    great=1;
+    bang=0;
+    cin>>a>>b;
+    bool flagg=false;
+    
+    for(int e=a+1;e<=62;e++){
+        for(int count=0;count<e;count++){
+            great*=2;
+        }
+        bang=great;
+        while(bang>=10){
+            bang/=10;
+        }
+        if(bang==b){
+            flagg=true;
+            cout<<e;
+            break;
+        }else{
+            great=1;
+        }
+        
+    }
+    if(flagg==false){
+        cout<<0;
+    }
+    
+    
+    
     return 0;
 }
